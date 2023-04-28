11cd15858042545b1eac3d7b83b651469203e919
Abbes Benayache <benayache.abbes@gmail.com>
2023-04-28 18:05:13 +0200

    validateInputs & extractZipAndBuildJson & sendEmail

---
+++ dapp/src/app.js
@@ -0,0 +1,62 @@
+const { promises: fs } = require("fs");
+const sendEmail = require("./emailService");
+const validateInputs = require("./validateInputs");
+const extractZipAndBuildJson = require("./extractJsonFromZip");
+require("dotenv").config();
+
+async function writeTaskOutput(path, message) {
+  try {
+    await fs.writeFile(path, message);
+  } catch (error) {
+    console.error(`Failed to write Task Output : ${error.message}`);
+    process.exit(1);
+  }
+}
+
+async function start() {
+  try {
+    const { MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE, MJ_SENDER } = JSON.parse(
+      process.env.IEXEC_APP_DEVELOPER_SECRET,
+    );
+    const envVars = {
+      iexecIn: process.env.IEXEC_IN,
+      iexecOut: process.env.IEXEC_OUT,
+      dataFileName: process.env.IEXEC_DATASET_FILENAME,
+      mailJetApiKeyPublic: MJ_APIKEY_PUBLIC,
+      mailJetApiKeyPrivate: MJ_APIKEY_PRIVATE,
+      mailJetSender: MJ_SENDER,
+      mailObject: process.env.IEXEC_REQUESTER_SECRET_1,
+      mailContent: process.env.IEXEC_REQUESTER_SECRET_2,
+    };
+    validateInputs(envVars);
+    const data = await extractZipAndBuildJson(
+      `${envVars.iexecIn}/${envVars.dataFileName}`,
+    );
+    if (!data.email) {
+      throw new Error("Missing email in protectedData");
+    }
+    await sendEmail({
+      email: data.email,
+      mailJetApiKeyPublic: envVars.mailJetApiKeyPublic,
+      mailJetApiKeyPrivate: envVars.mailJetApiKeyPrivate,
+      mailObject: envVars.mailObject,
+      mailContent: envVars.mailContent,
+      mailJetSender: envVars.mailJetSender,
+    });
+    await writeTaskOutput(
+      `${envVars.iexecOut}/result.txt`,
+      "Email sent successfully",
+    );
+    await writeTaskOutput(
+      `${envVars.iexecOut}/computed.json`,
+      JSON.stringify({
+        "deterministic-output-path": `${envVars.iexecOut}/result.txt`,
+      }),
+    );
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+start();
---
+++ dapp/src/validateInputs.js
@@ -0,0 +1,22 @@
+const Joi = require("joi");
+
+const schema = Joi.object({
+  iexecIn: Joi.string().required(),
+  iexecOut: Joi.string().required(),
+  dataFileName: Joi.string().required(),
+  mailJetApiKeyPublic: Joi.string().required(),
+  mailJetApiKeyPrivate: Joi.string().required(),
+  mailJetSender: Joi.string().email().required(),
+  mailObject: Joi.string().required(),
+  mailContent: Joi.string().required(),
+});
+
+function validateInputs(envVars) {
+  const { error } = schema.validate(envVars, { abortEarly: false });
+  if (error) {
+    const validationErrors = error.details.map((detail) => detail.message);
+    throw new Error(validationErrors.join("; "));
+  }
+}
+
+module.exports = validateInputs;
