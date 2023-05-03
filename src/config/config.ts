13a7ca834146c82155ad2464995b157a897b0976
SeddikBellamine <bellamine.seddik@gmail.com>
2023-05-03 16:51:25 +0200

    update config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,2 +1,3 @@
-export const WEB3_MAIL_DAPP_ADDRESS = '0x000000000';
+//TODO: add web3mail deployed dapp
+export const WEB3_MAIL_DAPP_ADDRESS = '0x000000000000000000000000000000000';
 export const ANY_DATASET = 'any';
