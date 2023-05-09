bc69600a1ffb79d58a2c00ecc692f8824423c025
Abbes Benayache <benayache.abbes@gmail.com>
2023-05-09 19:19:48 +0200

    refactor file path declaration in test

--- dapp/tests/e2e/sendEmail.test.js
+++ dapp/tests/e2e/sendEmail.test.js
@@ -1,13 +1,16 @@
 const sendEmail = require("../../src/emailService");
 const extractZipAndBuildJson = require("../../src/extractJsonFromZip");
+const path = require("path");
 
 describe("sendEmail", () => {
   it("should send an email successfully", async () => {
     // Place your .zip file in the /dapp/tests/_test_inputs_ directory
     // The .zip file should contain a file with the email content you want to protect
     // Define the absolute path of the .zip file containing the protected data
-    const zipPath =
-      "/home/abbes/iexec_workspace/github/web3Mail/dapp/tests/_test_inputs_/myProtectedData.zip";
+    const zipPath = path.join(
+      __dirname,
+      "../_test_inputs_/myProtectedData.zip",
+    );
     const result = await extractZipAndBuildJson(zipPath);
 
     const { MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE, MJ_SENDER } = JSON.parse(
