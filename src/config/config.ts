c84e72cd9b74a33ab7a24ac3ebf77a7cde45a106
Abbes Benayache <benayache.abbes@gmail.com>
2023-06-01 16:07:24 +0200

    update dapp config for prod environment

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,2 @@
-//TODO: add web3mail deployed dapp
-export const WEB3_MAIL_DAPP_ADDRESS =
-  '0x3ef531f670B12dc21490Fff2f81c20A59e17d254';
-export const WORKERPOOL_ADDRESS = 'debug-v8-bellecour.main.pools.iexec.eth';
+export const WEB3_MAIL_DAPP_ADDRESS = 'web3mail-dev.apps.iexec.eth';
+export const WORKERPOOL_ADDRESS = 'prod-v8-bellecour.main.pools.iexec.eth';
