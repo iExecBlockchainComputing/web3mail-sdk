5ab558bcdac9d437e2d0a6439c4c701de62692f0
Le-Caignec <robinlecaignec@yahoo.fr>
2023-06-12 16:47:05 +0200

    improve node demo

--- demo/node/index.js
+++ demo/node/index.js
@@ -1,12 +1,8 @@
-import { IExecWeb3Mail } from '@iexec/web3mail';
-import { getSignerFromPrivateKey } from 'iexec/utils';
+import { IExecWeb3Mail, getWeb3Provider } from '@iexec/web3mail';
 import { Wallet } from 'ethers';
 
 const test = async () => {
-  const ethProvider = getSignerFromPrivateKey(
-    'https://bellecour.iex.ec',
-    Wallet.createRandom().privateKey
-  );
+  const ethProvider = getWeb3Provider(Wallet.createRandom().privateKey);
 
   const web3Mail = new IExecWeb3Mail(ethProvider);
 
