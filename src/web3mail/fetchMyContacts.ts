5c20237f9eef61b2fdd833a057c2eec39cc900e6
Le-Caignec <robinlecaignec@yahoo.fr>
2023-07-06 14:54:40 +0200

    refactoring code

--- src/web3mail/fetchMyContacts.ts
+++ src/web3mail/fetchMyContacts.ts
@@ -1,9 +1,7 @@
-import { WEB3_MAIL_DAPP_ADDRESS } from '../config/config.js';
-import { WorkflowError } from '../utils/errors.js';
-import { autoPaginateRequest } from '../utils/paginate.js';
-import { getValidContact } from '../utils/subgraphQuery.js';
 import { throwIfMissing } from '../utils/validators.js';
 import { Contact, IExecConsumer, SubgraphConsumer } from './types.js';
+import { fetchUserContacts } from './fetchUserContacts.js';
+import { WorkflowError } from '../utils/errors.js';
 
 export const fetchMyContacts = async ({
   graphQLClient = throwIfMissing(),
@@ -11,38 +9,7 @@ export const fetchMyContacts = async ({
 }: IExecConsumer & SubgraphConsumer): Promise<Contact[]> => {
   try {
     const userAddress = await iexec.wallet.getAddress();
-    const showDatasetOrderbookRequest = iexec.orderbook.fetchDatasetOrderbook(
-      'any',
-      {
-        app: WEB3_MAIL_DAPP_ADDRESS,
-        requester: userAddress,
-      }
-    );
-    const { orders } = await autoPaginateRequest({
-      request: showDatasetOrderbookRequest,
-    });
-    let myContacts: Contact[] = [];
-    const web3DappResolvedAddress = await iexec.ens.resolveName(
-      WEB3_MAIL_DAPP_ADDRESS
-    );
-
-    orders.forEach((order) => {
-      if (
-        order.order.apprestrict.toLowerCase() ===
-        web3DappResolvedAddress.toLowerCase()
-      ) {
-        const contact = {
-          address: order.order.dataset.toLowerCase(),
-          owner: order.signer.toLowerCase(),
-          accessGrantTimestamp: order.publicationTimestamp,
-        };
-        myContacts.push(contact);
-      }
-    });
-
-    const validContacts = await getValidContact(graphQLClient, myContacts);
-
-    return validContacts;
+    return fetchUserContacts({ graphQLClient, iexec, userAddress });
   } catch (error) {
     throw new WorkflowError(
       `Failed to fetch my contacts: ${error.message}`,
