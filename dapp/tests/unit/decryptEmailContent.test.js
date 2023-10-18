780dd458250ae8c7965d37defc6c4ba4d73d4ec3
Le-Caignec <robinlecaignec@yahoo.fr>
2023-10-18 14:54:14 +0200

    remove useless tests

--- dapp/tests/unit/decryptEmailContent.test.js
+++ dapp/tests/unit/decryptEmailContent.test.js
@@ -2,6 +2,7 @@ const {
   decryptContent,
   downloadEncryptedContent,
 } = require('../../src/decryptEmailContent');
+
 describe('decryptContent', () => {
   it('should decrypt content correctly', async () => {
     const { IExec } = await import('iexec');
@@ -25,7 +26,6 @@ describe('decryptContent', () => {
   });
 });
 const DEFAULT_IPFS_GATEWAY = 'https://ipfs-gateway.v8-bellecour.iex.ec';
-const IPFS_UPLOAD_URL = '/dns4/ipfs-upload.v8-bellecour.iex.ec/https';
 
 describe('downloadEncryptedContent', () => {
   it('should return the encrypted content', async () => {
