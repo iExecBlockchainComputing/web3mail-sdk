7dd7dec801755d36fa42720a3af909cbb733255b
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-11-02 10:17:29 +0100

    import 'ValidationError' from 'yup' I guess

--- tests/unit/utils/validators.test.ts
+++ tests/unit/utils/validators.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from '@jest/globals';
-import { ValidationError } from '../../../src/utils/errors';
+import { ValidationError } from 'yup';
 import {
   addressOrEnsSchema,
   emailSubjectSchema,
