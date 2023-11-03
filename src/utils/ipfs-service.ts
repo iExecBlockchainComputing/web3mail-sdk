05a6352e0d76ae3c210e62f63613aadcd9eee71f
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-11-03 15:55:59 +0100

    Keep existing kubo-rpc-client create call and just make TS silent

--- src/utils/ipfs-service.ts
+++ src/utils/ipfs-service.ts
@@ -29,6 +29,7 @@ const add = async (
     ipfsGateway = DEFAULT_IPFS_GATEWAY,
   }: AddOptions = {}
 ) => {
+  // @ts-ignore
   const ipfsClient = create(ipfsNode);
   const { cid } = await ipfsClient.add(content);
   await get(cid.toString(), { ipfsGateway });
