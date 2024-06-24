6677c3a6fc3b6f6b29b2c3c2282752aa337dd909
Le-Caignec <robinlecaignec@yahoo.fr>
2024-06-24 11:32:26 +0200

    export WorkflowError type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { IExecWeb3mail } from './web3mail/IExecWeb3mail.js';
 export * from './web3mail/types.js';
+export * from './utils/index.js';
 export { getWeb3Provider } from './utils/getWeb3Provider.js';
