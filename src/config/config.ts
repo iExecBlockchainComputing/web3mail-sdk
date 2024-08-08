5f67a52762daf31a735eb2c560526ffe1fd7a704
Abbes Benayache <benayache.abbes@gmail.com>
2024-08-08 15:50:25 +0200

    update env config (bubble, staging, prod)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,72 @@
-export const WEB3_MAIL_DAPP_ADDRESS = 'web3mail.apps.iexec.eth';
-export const PROD_WORKERPOOL_ADDRESS = 'prod-v8-bellecour.main.pools.iexec.eth';
-export const DATAPROTECTOR_SUBGRAPH_ENDPOINT =
-  'https://thegraph-product.iex.ec/subgraphs/name/bellecour/dataprotector';
+import { KnownEnv, getEnvironment } from '@iexec/web3mail-environments';
+import 'dotenv/config';
+
+const { ENV } = process.env;
+
+const {
+  chainId,
+  rpcURL,
+  hubAddress,
+  ensRegistryAddress,
+  ensPublicResolverAddress,
+  voucherHubAddress,
+  smsURL,
+  iexecGatewayURL,
+  resultProxyURL,
+  ipfsGatewayURL,
+  ipfsNodeURL,
+  pocoSubgraphURL,
+  voucherSubgraphURL,
+  dappAddressOrENS,
+  dappWhitelistAddress,
+  dataprotectorContractAddress,
+  dataProtectorSubgraph,
+  workerpoolProdAddress,
+} = getEnvironment(ENV as KnownEnv);
+
+export const iexecOptions = {
+  chainId,
+  rpcURL,
+  hubAddress,
+  ensRegistryAddress,
+  ensPublicResolverAddress,
+  voucherHubAddress,
+  smsURL,
+  iexecGatewayURL,
+  resultProxyURL,
+  ipfsGatewayURL,
+  ipfsNodeURL,
+  pocoSubgraphURL,
+  voucherSubgraphURL,
+};
+
+export const web3mailOptions = {
+  dappAddressOrENS,
+  dappWhitelistAddress,
+  dataProtectorSubgraph,
+  ipfsNode: ipfsNodeURL,
+  ipfsGateway: ipfsGatewayURL,
+};
+export const dataProtectorOptions = {
+  contractAddress: dataprotectorContractAddress,
+  subgraphUrl: dataProtectorSubgraph,
+  ipfsNode: ipfsNodeURL,
+  ipfsGateway: ipfsGatewayURL,
+};
+export const WEB3_MAIL_DAPP_ADDRESS = dappAddressOrENS;
+
+export const PROD_WORKERPOOL_ADDRESS = workerpoolProdAddress;
+
+export const DATAPROTECTOR_SUBGRAPH_ENDPOINT = dataProtectorSubgraph;
+
 export const MAX_DESIRED_DATA_ORDER_PRICE = 0;
 export const MAX_DESIRED_APP_ORDER_PRICE = 0;
 export const MAX_DESIRED_WORKERPOOL_ORDER_PRICE = 0;
 export const DEFAULT_CONTENT_TYPE = 'text/plain';
-export const IPFS_UPLOAD_URL = '/dns4/ipfs-upload.v8-bellecour.iex.ec/https';
-export const DEFAULT_IPFS_GATEWAY = 'https://ipfs-gateway.v8-bellecour.iex.ec';
-export const WHITELIST_SMART_CONTRACT_ADDRESS =
-  '0x781482C39CcE25546583EaC4957Fb7Bf04C277D2';
+export const IPFS_UPLOAD_URL = ipfsNodeURL;
+
+export const DEFAULT_IPFS_GATEWAY = ipfsGatewayURL;
+
+export const WHITELIST_SMART_CONTRACT_ADDRESS = dappWhitelistAddress;
+
 export const ANY_DATASET_ADDRESS = 'any';
