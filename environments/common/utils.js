a6e7f2052cce6c8f0a34c92d53d460d62dc15004
Abbes Benayache <benayache.abbes@gmail.com>
2024-08-16 15:11:53 +0200

    add web3mail-environments package

---
+++ environments/common/utils.js
@@ -0,0 +1,27 @@
+// when editing this file update types/index.d.ts
+
+export const KNOWN_ENVS = ['prod', 'staging', 'bubble', 'bellecour-fork'];
+
+export const KNOWN_KEYS = [
+  'chainId',
+  'rpcURL',
+  'hubAddress',
+  'ensRegistryAddress',
+  'ensPublicResolverAddress',
+  'voucherHubAddress',
+  'smsURL',
+  'iexecGatewayURL',
+  'resultProxyURL',
+  'ipfsGatewayURL',
+  'ipfsNodeURL',
+  'pocoSubgraphURL',
+  'voucherSubgraphURL',
+
+  'dappAddressOrENS',
+  'dappWhitelistAddress',
+  'dataProtectorSubgraph',
+  'ipfsNode',
+  'ipfsGateway',
+
+  'workerpool',
+];
