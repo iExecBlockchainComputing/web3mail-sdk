f14334d10e1b1f758f104d40f3bbbcb5a316bd87
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2024-10-17 09:46:47 +0200

    Change the way iexec SDK is initiated

--- tests/unit/fetchMyContacts.test.ts
+++ tests/unit/fetchMyContacts.test.ts
@@ -1,38 +1,51 @@
 import { describe, expect, it, jest } from '@jest/globals';
+import { HDNodeWallet, Wallet } from 'ethers';
 import { GraphQLClient } from 'graphql-request';
-import { IExec } from 'iexec';
+import { IExec, utils } from 'iexec';
 import {
   DATAPROTECTOR_SUBGRAPH_ENDPOINT,
   WEB3_MAIL_DAPP_ADDRESS,
   WHITELIST_SMART_CONTRACT_ADDRESS,
 } from '../../src/config/config.js';
 import { fetchMyContacts } from '../../src/web3mail/fetchMyContacts.js';
-import { getTestWeb3SignerProvider } from '../test-utils.js';
+
+const MOCK_ORDER = {
+  order: {
+    dataset: '0x35396912Db97ff130411301Ec722Fc92Ac37B00d',
+    datasetprice: 0,
+    volume: 10,
+    tag: '0x0000000000000000000000000000000000000000000000000000000000000003',
+    apprestrict: '0x0000000000000000000000000000000000000000',
+    workerpoolrestrict: '0x0000000000000000000000000000000000000000',
+    requesterrestrict: '0x0000000000000000000000000000000000000000',
+    salt: '0x2a366726dc6321e78bba6697102f5953ceccfe6c0ddf9499dbb49c99bac1c16d',
+    sign: '0xb00707c4be504e6e07d20bd2e52babd72cbd26f064ec7648c6b684578232bee255a9c98aa2e9b18b4088602967d4f0641d52c0fbb3d5c00304a1f6d3c19eaf4f1c',
+  },
+  orderHash:
+    '0x396392835c2cbe933023dd28a3d6eedceb21c52b1dba199835a6f24cc75e7685',
+  chainId: 134,
+  publicationTimestamp: '2023-06-15T16:39:22.713Z',
+  signer: '0xD52C27CC2c7D3fb5BA4440ffa825c12EA5658D60',
+  status: 'open',
+  remaining: 10,
+};
 
 describe('fetchMyContacts', () => {
-  const MOCK_ORDER = {
-    order: {
-      dataset: '0x35396912Db97ff130411301Ec722Fc92Ac37B00d',
-      datasetprice: 0,
-      volume: 10,
-      tag: '0x0000000000000000000000000000000000000000000000000000000000000003',
-      apprestrict: '0x0000000000000000000000000000000000000000',
-      workerpoolrestrict: '0x0000000000000000000000000000000000000000',
-      requesterrestrict: '0x0000000000000000000000000000000000000000',
-      salt: '0x2a366726dc6321e78bba6697102f5953ceccfe6c0ddf9499dbb49c99bac1c16d',
-      sign: '0xb00707c4be504e6e07d20bd2e52babd72cbd26f064ec7648c6b684578232bee255a9c98aa2e9b18b4088602967d4f0641d52c0fbb3d5c00304a1f6d3c19eaf4f1c',
-    },
-    orderHash:
-      '0x396392835c2cbe933023dd28a3d6eedceb21c52b1dba199835a6f24cc75e7685',
-    chainId: 134,
-    publicationTimestamp: '2023-06-15T16:39:22.713Z',
-    signer: '0xD52C27CC2c7D3fb5BA4440ffa825c12EA5658D60',
-    status: 'open',
-    remaining: 10,
-  };
+  let wallet: HDNodeWallet;
+  let iexec: IExec;
+
+  beforeEach(async () => {
+    wallet = Wallet.createRandom();
+    iexec = new IExec({
+      ethProvider: utils.getSignerFromPrivateKey(
+        'https://bellecour.iex.ec',
+        wallet.privateKey
+      ),
+    });
+  });
+
   it('should fetch granted access without parameters (using default parameters)', async () => {
     const graphQLClient = new GraphQLClient(DATAPROTECTOR_SUBGRAPH_ENDPOINT);
-    const iexec = new IExec({ ethProvider: getTestWeb3SignerProvider() });
     const mockFetchDatasetOrderbook: any = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         ok: true,
@@ -44,7 +57,7 @@ describe('fetchMyContacts', () => {
     iexec.orderbook.fetchDatasetOrderbook = mockFetchDatasetOrderbook;
 
     await fetchMyContacts({
-      iexec: iexec,
+      iexec,
       dappAddressOrENS: WEB3_MAIL_DAPP_ADDRESS,
       dappWhitelistAddress: WHITELIST_SMART_CONTRACT_ADDRESS,
       graphQLClient,
@@ -76,7 +89,6 @@ describe('fetchMyContacts', () => {
 
   it('should fetch granted access with isRequesterStrict param equal to true', async () => {
     const graphQLClient = new GraphQLClient(DATAPROTECTOR_SUBGRAPH_ENDPOINT);
-    const iexec = new IExec({ ethProvider: getTestWeb3SignerProvider() });
     const mockFetchDatasetOrderbook: any = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         ok: true,
