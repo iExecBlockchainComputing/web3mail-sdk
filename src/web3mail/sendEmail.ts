730125c086676faf1fe86c264133f03dd6cd6841
SeddikBellamine <bellamine.seddik@gmail.com>
2025-11-05 21:17:01 +0100

    refactor: restore sendEmail to original single email sending implementation
    - Remove bulk processing logic from sendEmail
    - Restore orderbook-based single email sending
    - Bulk processing now handled by prepareEmailCampaign and sendEmailCampaign methods

--- src/web3mail/sendEmail.ts
+++ src/web3mail/sendEmail.ts
@@ -1,15 +1,18 @@
 import { Buffer } from 'buffer';
 import {
+  CALLBACK_WEB3MAIL,
   DEFAULT_CONTENT_TYPE,
   MAX_DESIRED_APP_ORDER_PRICE,
   MAX_DESIRED_DATA_ORDER_PRICE,
   MAX_DESIRED_WORKERPOOL_ORDER_PRICE,
 } from '../config/config.js';
 import { handleIfProtocolError, WorkflowError } from '../utils/errors.js';
+import { generateSecureUniqueId } from '../utils/generateUniqueId.js';
 import * as ipfs from '../utils/ipfs-service.js';
 import { checkProtectedDataValidity } from '../utils/subgraphQuery.js';
 import {
   addressOrEnsSchema,
+  addressSchema,
   booleanSchema,
   contentTypeSchema,
   emailContentSchema,
@@ -19,28 +22,30 @@ import {
   senderNameSchema,
   throwIfMissing,
 } from '../utils/validators.js';
-import { SendEmailParams, SendEmailSingleResponse } from './types.js';
+import {
+  checkUserVoucher,
+  filterWorkerpoolOrders,
+} from './sendEmail.models.js';
+import { SendEmailParams, SendEmailResponse } from './types.js';
 import {
   DappAddressConsumer,
   DappWhitelistAddressConsumer,
-  DataProtectorConsumer,
   IExecConsumer,
   IpfsGatewayConfigConsumer,
   IpfsNodeConfigConsumer,
   SubgraphConsumer,
 } from './internalTypes.js';
-import { ProcessBulkRequestResponse } from '@iexec/dataprotector';
+
 export type SendEmail = typeof sendEmail;
 
 export const sendEmail = async ({
   graphQLClient = throwIfMissing(),
   iexec = throwIfMissing(),
-  dataProtector = throwIfMissing(),
-  workerpoolAddressOrEns = throwIfMissing(),
+  workerpoolAddressOrEns,
   dappAddressOrENS,
+  dappWhitelistAddress,
   ipfsNode,
   ipfsGateway,
-  senderName,
   emailSubject,
   emailContent,
   contentType = DEFAULT_CONTENT_TYPE,
@@ -48,9 +53,8 @@ export const sendEmail = async ({
   dataMaxPrice = MAX_DESIRED_DATA_ORDER_PRICE,
   appMaxPrice = MAX_DESIRED_APP_ORDER_PRICE,
   workerpoolMaxPrice = MAX_DESIRED_WORKERPOOL_ORDER_PRICE,
+  senderName,
   protectedData,
-  grantedAccess,
-  maxProtectedDataPerTask,
   useVoucher = false,
 }: IExecConsumer &
   SubgraphConsumer &
@@ -58,56 +62,200 @@ export const sendEmail = async ({
   DappWhitelistAddressConsumer &
   IpfsNodeConfigConsumer &
   IpfsGatewayConfigConsumer &
-  SendEmailParams &
-  DataProtectorConsumer): Promise<
-  ProcessBulkRequestResponse | SendEmailSingleResponse
-> => {
-  try {
-    const vUseVoucher = booleanSchema()
-      .label('useVoucher')
-      .validateSync(useVoucher);
-    const vWorkerpoolAddressOrEns = addressOrEnsSchema()
-      .required()
-      .label('WorkerpoolAddressOrEns')
-      .validateSync(workerpoolAddressOrEns);
-    const vSenderName = senderNameSchema()
-      .label('senderName')
-      .validateSync(senderName);
-    const vEmailSubject = emailSubjectSchema()
-      .required()
-      .label('emailSubject')
-      .validateSync(emailSubject);
-    const vEmailContent = emailContentSchema()
-      .required()
-      .label('emailContent')
-      .validateSync(emailContent);
-    const vContentType = contentTypeSchema()
-      .required()
-      .label('contentType')
-      .validateSync(contentType);
-    const vLabel = labelSchema().label('label').validateSync(label);
-    const vDappAddressOrENS = addressOrEnsSchema()
-      .required()
-      .label('dappAddressOrENS')
-      .validateSync(dappAddressOrENS);
-    // TODO: remove this once we have a way to pass appWhitelist to processProtectedData function
-    // const vDappWhitelistAddress = addressSchema()
-    //   .required()
-    //   .label('dappWhitelistAddress')
-    //   .validateSync(dappWhitelistAddress);
-    // Note: Price parameters may not be directly supported in processProtectedData
-    // They might be handled differently in the dataprotector SDK
-    const vAppMaxPrice = positiveNumberSchema()
-      .label('appMaxPrice')
-      .validateSync(appMaxPrice);
-    const vDataMaxPrice = positiveNumberSchema()
-      .label('dataMaxPrice')
-      .validateSync(dataMaxPrice);
-    const vWorkerpoolMaxPrice = positiveNumberSchema()
-      .label('workerpoolMaxPrice')
-      .validateSync(workerpoolMaxPrice);
+  SendEmailParams): Promise<SendEmailResponse> => {
+  const vDatasetAddress = addressOrEnsSchema()
+    .required()
+    .label('protectedData')
+    .validateSync(protectedData);
 
-    // Encrypt email content
+  const vEmailSubject = emailSubjectSchema()
+    .required()
+    .label('emailSubject')
+    .validateSync(emailSubject);
+
+  const vEmailContent = emailContentSchema()
+    .required()
+    .label('emailContent')
+    .validateSync(emailContent);
+
+  const vContentType = contentTypeSchema()
+    .required()
+    .label('contentType')
+    .validateSync(contentType);
+
+  const vSenderName = senderNameSchema()
+    .label('senderName')
+    .validateSync(senderName);
+
+  const vLabel = labelSchema().label('label').validateSync(label);
+
+  const vWorkerpoolAddressOrEns = addressOrEnsSchema()
+    .required()
+    .label('WorkerpoolAddressOrEns')
+    .validateSync(workerpoolAddressOrEns);
+
+  const vDappAddressOrENS = addressOrEnsSchema()
+    .required()
+    .label('dappAddressOrENS')
+    .validateSync(dappAddressOrENS);
+
+  const vDappWhitelistAddress = addressSchema()
+    .required()
+    .label('dappWhitelistAddress')
+    .validateSync(dappWhitelistAddress);
+
+  const vDataMaxPrice = positiveNumberSchema()
+    .label('dataMaxPrice')
+    .validateSync(dataMaxPrice);
+
+  const vAppMaxPrice = positiveNumberSchema()
+    .label('appMaxPrice')
+    .validateSync(appMaxPrice);
+
+  const vWorkerpoolMaxPrice = positiveNumberSchema()
+    .label('workerpoolMaxPrice')
+    .validateSync(workerpoolMaxPrice);
+
+  const vUseVoucher = booleanSchema()
+    .label('useVoucher')
+    .validateSync(useVoucher);
+
+  // Check protected data schema through subgraph
+  const isValidProtectedData = await checkProtectedDataValidity(
+    graphQLClient,
+    vDatasetAddress
+  );
+
+  if (!isValidProtectedData) {
+    throw new Error(
+      'This protected data does not contain "email:string" in its schema.'
+    );
+  }
+
+  const requesterAddress = await iexec.wallet.getAddress();
+
+  let userVoucher;
+  if (vUseVoucher) {
+    try {
+      userVoucher = await iexec.voucher.showUserVoucher(requesterAddress);
+      checkUserVoucher({ userVoucher });
+    } catch (err) {
+      if (err?.message?.startsWith('No Voucher found for address')) {
+        throw new Error(
+          'Oops, it seems your wallet is not associated with any voucher. Check on https://builder.iex.ec/'
+        );
+      }
+      throw err;
+    }
+  }
+
+  try {
+    const [
+      datasetorderForApp,
+      datasetorderForWhitelist,
+      apporder,
+      workerpoolorder,
+    ] = await Promise.all([
+      // Fetch dataset order for web3mail app
+      iexec.orderbook
+        .fetchDatasetOrderbook(vDatasetAddress, {
+          app: dappAddressOrENS,
+          requester: requesterAddress,
+        })
+        .then((datasetOrderbook) => {
+          const desiredPriceDataOrderbook = datasetOrderbook.orders.filter(
+            (order) => order.order.datasetprice <= vDataMaxPrice
+          );
+          return desiredPriceDataOrderbook[0]?.order; // may be undefined
+        }),
+
+      // Fetch dataset order for web3mail whitelist
+      iexec.orderbook
+        .fetchDatasetOrderbook(vDatasetAddress, {
+          app: vDappWhitelistAddress,
+          requester: requesterAddress,
+        })
+        .then((datasetOrderbook) => {
+          const desiredPriceDataOrderbook = datasetOrderbook.orders.filter(
+            (order) => order.order.datasetprice <= vDataMaxPrice
+          );
+          return desiredPriceDataOrderbook[0]?.order; // may be undefined
+        }),
+
+      // Fetch app order
+      iexec.orderbook
+        .fetchAppOrderbook(dappAddressOrENS, {
+          minTag: ['tee', 'scone'],
+          maxTag: ['tee', 'scone'],
+          workerpool: workerpoolAddressOrEns,
+        })
+        .then((appOrderbook) => {
+          const desiredPriceAppOrderbook = appOrderbook.orders.filter(
+            (order) => order.order.appprice <= vAppMaxPrice
+          );
+          const desiredPriceAppOrder = desiredPriceAppOrderbook[0]?.order;
+          if (!desiredPriceAppOrder) {
+            throw new Error('No App order found for the desired price');
+          }
+          return desiredPriceAppOrder;
+        }),
+
+      // Fetch workerpool order for App or AppWhitelist
+      Promise.all([
+        // for app
+        iexec.orderbook.fetchWorkerpoolOrderbook({
+          workerpool: workerpoolAddressOrEns,
+          app: vDappAddressOrENS,
+          dataset: vDatasetAddress,
+          requester: requesterAddress, // public orders + user specific orders
+          isRequesterStrict: useVoucher, // If voucher, we only want user specific orders
+          minTag: ['tee', 'scone'],
+          maxTag: ['tee', 'scone'],
+          category: 0,
+        }),
+        // for app whitelist
+        iexec.orderbook.fetchWorkerpoolOrderbook({
+          workerpool: workerpoolAddressOrEns,
+          app: vDappWhitelistAddress,
+          dataset: vDatasetAddress,
+          requester: requesterAddress, // public orders + user specific orders
+          isRequesterStrict: useVoucher, // If voucher, we only want user specific orders
+          minTag: ['tee', 'scone'],
+          maxTag: ['tee', 'scone'],
+          category: 0,
+        }),
+      ]).then(
+        ([workerpoolOrderbookForApp, workerpoolOrderbookForAppWhitelist]) => {
+          const desiredPriceWorkerpoolOrder = filterWorkerpoolOrders({
+            workerpoolOrders: [
+              ...workerpoolOrderbookForApp.orders,
+              ...workerpoolOrderbookForAppWhitelist.orders,
+            ],
+            workerpoolMaxPrice: vWorkerpoolMaxPrice,
+            useVoucher: vUseVoucher,
+            userVoucher,
+          });
+
+          if (!desiredPriceWorkerpoolOrder) {
+            throw new Error('No Workerpool order found for the desired price');
+          }
+
+          return desiredPriceWorkerpoolOrder;
+        }
+      ),
+    ]);
+
+    if (!workerpoolorder) {
+      throw new Error('No Workerpool order found for the desired price');
+    }
+
+    const datasetorder = datasetorderForApp || datasetorderForWhitelist;
+    if (!datasetorder) {
+      throw new Error('No Dataset order found for the desired price');
+    }
+
+    // Push requester secrets
+    const requesterSecretId = generateSecureUniqueId(16);
     const emailContentEncryptionKey = iexec.dataset.generateEncryptionKey();
     const encryptedFile = await iexec.dataset
       .encrypt(Buffer.from(vEmailContent, 'utf8'), emailContentEncryptionKey)
@@ -118,11 +266,10 @@ export const sendEmail = async ({
         });
       });
 
-    // Push email content to IPFS
     const cid = await ipfs
       .add(encryptedFile, {
-        ipfsNode,
-        ipfsGateway,
+        ipfsNode: ipfsNode,
+        ipfsGateway: ipfsGateway,
       })
       .catch((e) => {
         throw new WorkflowError({
@@ -130,118 +277,60 @@ export const sendEmail = async ({
           errorCause: e,
         });
       });
+
     const multiaddr = `/ipfs/${cid}`;
 
-    // Prepare secrets for the requester
-    // Use a positive integer as secret ID (required by iexec)
-    // Using "1" as a fixed ID for the requester secret
-    const requesterSecretId = 1;
-    const secrets = {
-      [requesterSecretId]: JSON.stringify({
+    await iexec.secrets.pushRequesterSecret(
+      requesterSecretId,
+      JSON.stringify({
         emailSubject: vEmailSubject,
         emailContentMultiAddr: multiaddr,
         contentType: vContentType,
         senderName: vSenderName,
         emailContentEncryptionKey,
         useCallback: true,
-      }),
-    };
-
-    // Bulk processing
-    if (grantedAccess) {
-      const vMaxProtectedDataPerTask = positiveNumberSchema()
-        .label('maxProtectedDataPerTask')
-        .validateSync(maxProtectedDataPerTask);
-
-      const bulkRequest = await (dataProtector as any).prepareBulkRequest({
-        app: vDappAddressOrENS,
-        appMaxPrice: vAppMaxPrice,
-        workerpoolMaxPrice: vWorkerpoolMaxPrice,
-        workerpool: vWorkerpoolAddressOrEns,
-        args: vLabel,
-        inputFiles: [],
-        secrets,
-        bulkOrders: grantedAccess,
-        maxProtectedDataPerTask: vMaxProtectedDataPerTask,
-      });
-
-      const processBulkRequestResponse: ProcessBulkRequestResponse = await (
-        dataProtector as any
-      ).processBulkRequest({
-        bulkRequest: bulkRequest.bulkRequest,
-        useVoucher: vUseVoucher,
-        workerpool: vWorkerpoolAddressOrEns,
-      });
-
-      return processBulkRequestResponse;
-    }
-
-    // Single processing mode - protectedData is required
-    const vDatasetAddress = addressOrEnsSchema()
-      .required()
-      .label('protectedData')
-      .validateSync(protectedData);
-
-    // Check protected data validity through subgraph
-    const isValidProtectedData = await checkProtectedDataValidity(
-      graphQLClient,
-      vDatasetAddress
+      })
     );
 
-    if (!isValidProtectedData) {
-      throw new Error(
-        'This protected data does not contain "email:string" in its schema.'
-      );
-    }
-
-    // Use processProtectedData from dataprotector
-    // Note: Some parameters may need to be adjusted based on actual dataprotector SDK API
-    const result = await dataProtector.processProtectedData({
-      defaultWorkerpool: vWorkerpoolAddressOrEns,
-      protectedData: vDatasetAddress,
+    const requestorderToSign = await iexec.order.createRequestorder({
       app: vDappAddressOrENS,
-      // userWhitelist: vDappWhitelistAddress, // Removed due to bug in dataprotector v2.0.0-beta.20
+      category: workerpoolorder.category,
+      dataset: vDatasetAddress,
+      datasetmaxprice: datasetorder.datasetprice,
+      appmaxprice: apporder.appprice,
+      workerpoolmaxprice: workerpoolorder.workerpoolprice,
+      tag: ['tee', 'scone'],
       workerpool: vWorkerpoolAddressOrEns,
-      workerpoolMaxPrice: vWorkerpoolMaxPrice,
-      dataMaxPrice: vDataMaxPrice,
-      appMaxPrice: vAppMaxPrice,
-      args: vLabel,
-      inputFiles: [],
-      secrets,
-      useVoucher: vUseVoucher,
-      waitForResult: false,
+      callback: CALLBACK_WEB3MAIL,
+      params: {
+        iexec_secrets: {
+          1: requesterSecretId,
+        },
+        iexec_args: vLabel,
+      },
     });
 
+    const requestorder = await iexec.order.signRequestorder(requestorderToSign);
+
+    // Match orders and compute task ID
+    const { dealid } = await iexec.order.matchOrders(
+      {
+        apporder: apporder,
+        datasetorder: datasetorder,
+        workerpoolorder: workerpoolorder,
+        requestorder: requestorder,
+      },
+      { useVoucher: vUseVoucher }
+    );
+
+    const taskId = await iexec.deal.computeTaskId(dealid, 0);
+
     return {
-      taskId: result.taskId,
+      taskId,
     };
   } catch (error) {
-    // Protocol error detected, re-throwing as-is
-    if ((error as any)?.isProtocolError === true) {
-      throw error;
-    }
-
-    // Handle protocol errors - this will throw if it's an ApiCallError
-    // handleIfProtocolError transforms ApiCallError into a WorkflowError with isProtocolError=true
     handleIfProtocolError(error);
 
-    const isProcessProtectedDataError =
-      error instanceof Error &&
-      error.message === 'Failed to process protected data';
-
-    if (isProcessProtectedDataError) {
-      const cause = (error as any)?.cause;
-      // Return unwrapped cause (the actual Error object)
-      // error.cause should be an Error, but ensure it is
-      const unwrappedCause = cause instanceof Error ? cause : error;
-
-      throw new WorkflowError({
-        message: 'Failed to sendEmail',
-        errorCause: unwrappedCause,
-      });
-    }
-
-    // For all other errors
     throw new WorkflowError({
       message: 'Failed to sendEmail',
       errorCause: error,
